c05df86892dff0bba46ef8b1ab4b0ca2ad8bb985
Eduardo Emilio Montejo Benavides - Ceiba Software <eduardo.montejo@ceiba.com.co>
2019-06-20 11:38:16 -0500

    Se elimina testBase

--- domain/src/main/java/co/com/ceiba/estacionamiento/domain/entity/ValidatorArgument.java
+++ domain/src/main/java/co/com/ceiba/estacionamiento/domain/entity/ValidatorArgument.java
@@ -4,6 +4,10 @@ import co.com.ceiba.estacionamiento.domain.exception.ParkingLotException;
 
 public class ValidatorArgument {
 	
+	private ValidatorArgument() {
+		//
+	}
+	
 	public static void validateRequired(Object value, String message) {
 		if(value == null) {
 			throw new ParkingLotException(message);
