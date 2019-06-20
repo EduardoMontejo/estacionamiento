87bd3ae0ebce54243a9f1e8effa5c4114b0fa9d2
Eduardo Emilio Montejo Benavides - Ceiba Software <eduardo.montejo@ceiba.com.co>
2019-06-20 18:43:52 -0500

    Se agregan pruebas unitarias al servicio de registrar vehiculo

--- domain/src/test/java/co/com/ceiba/estacionamiento/domain/service/ServiceVehicleEntryTest.java
+++ domain/src/test/java/co/com/ceiba/estacionamiento/domain/service/ServiceVehicleEntryTest.java
@@ -1,12 +1,18 @@
 package co.com.ceiba.estacionamiento.domain.service;
 
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import co.com.ceiba.estacionamiento.domain.entity.Ticket;
 import co.com.ceiba.estacionamiento.domain.entity.Vehicle;
 import co.com.ceiba.estacionamiento.domain.exception.ParkingLotException;
 import co.com.ceiba.estacionamiento.domain.port.ParkingLotRepository;
+import co.com.ceiba.estacionamiento.domain.testdatabuilder.TicketTestDataBuilder;
 import co.com.ceiba.estacionamiento.domain.testdatabuilder.VehicleTestDataBuilder;
 
 public class ServiceVehicleEntryTest {
@@ -20,5 +26,51 @@ public class ServiceVehicleEntryTest {
 		
 		//act - assert
 		service.registerVehicleEntry(vehicle);
+	}
+	
+	@Test
+	public void countMotorcycles() {
+		//arrange
+		ParkingLotRepository repository = Mockito.mock(ParkingLotRepository.class);
+		ServiceRegisterVehicleEntry service = new ServiceRegisterVehicleEntry(repository);
+		
+		Collection<Ticket> tickets = new ArrayList<>();
+		
+		for(int i = 0; i < 10; i++) {
+			Vehicle newVehicle = new VehicleTestDataBuilder().withPlate("WSN25C").withTypeVehicle("moto").build();
+			Ticket newTicket = new TicketTestDataBuilder().withVehicle(newVehicle).build();
+			tickets.add(newTicket);
+		}
+		
+		Mockito.when(repository.findVehiclesInParkingLot()).thenReturn(tickets);
+		
+		//act
+		int numMotorcycle = service.countVehicles("moto");
+		
+		//assert
+		assertTrue(10 == numMotorcycle);
+	}
+	
+	@Test
+	public void countCars() {
+		//arrange
+		ParkingLotRepository repository = Mockito.mock(ParkingLotRepository.class);
+		ServiceRegisterVehicleEntry service = new ServiceRegisterVehicleEntry(repository);
+		
+		Collection<Ticket> tickets = new ArrayList<>();
+		
+		for(int i = 0; i < 20; i++) {
+			Vehicle newVehicle = new VehicleTestDataBuilder().withPlate("WSN25C").withTypeVehicle("carro").build();
+			Ticket newTicket = new TicketTestDataBuilder().withVehicle(newVehicle).build();
+			tickets.add(newTicket);
+		}
+		
+		Mockito.when(repository.findVehiclesInParkingLot()).thenReturn(tickets);
+		
+		//act
+		int numMotorcycle = service.countVehicles("carro");
+		
+		//assert
+		assertTrue(20 == numMotorcycle);
 	}
 }
